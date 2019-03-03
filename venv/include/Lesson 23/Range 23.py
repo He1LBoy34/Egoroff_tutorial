e60448bb0f9dfdea1ccf9f14ee1c4f275099b232
Nikolai Timofeev <extezy34@gmail.com>
2019-03-03 13:36:00 +0300

    Lesson 23

---
+++ venv/include/Lesson 23/Range 23.py
@@ -0,0 +1,18 @@
+i = range(5)
+print(type(i))
+print(i)
+print(list(i))
+
+print(list(range(10, 30)))  # правый интервал не включительно
+
+print(list(range(100, 10, -10)))    # третье число шаг с которым идёт последовательность
+
+print(sum(range(10)))
+
+print(len(range(3)))
+
+a, b, c = range(2, 3), range(1), range(8)
+print(a, b, c)
+
+
+
