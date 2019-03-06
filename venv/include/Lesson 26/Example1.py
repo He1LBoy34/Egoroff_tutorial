384f2210b42b7fda9866b07468102580154a7c1c
Nikolai Timofeev <extezy34@gmail.com>
2019-03-06 22:12:38 +0300

    Lesson 26

---
+++ venv/include/Lesson 26/Example1.py
@@ -0,0 +1,15 @@
+print('Exaple')
+print('Exaple')
+print('Exaple')
+print('Exaple')
+print('Exaple')
+print('Exaple')
+print('Exaple')
+print('Exaple')
+
+s = "Hello World"
+
+
+print(s.upper())
+
+# ctrl + / коментирует строку
---
+++ venv/include/Lesson 26/exmaple 2.py
@@ -0,0 +1,15 @@
+if 4 > 2:
+    print(4)
+    print(6)
+else:
+    print(5)
+    print(6)
+print(99)
+
+for i in range(1, 5):
+    print(i)
+
+a = [3, 3, 4, 2, 3, 4, 3, 3, 4]
+
+while 3 in a:
+    a.remove(3)
