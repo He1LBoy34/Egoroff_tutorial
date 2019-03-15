155e1d8f44e0981a4167bd49bdfff91c1f96ad56
Nikolai Timofeev <extezy34@gmail.com>
2019-03-15 23:20:35 +0300

    Lesson 33

---
+++ venv/include/Lesson 33/Подсчёт объектов.py
@@ -0,0 +1,12 @@
+# !. Словарь полезно использовать при подсчёте объектов
+s = input()
+d = {}
+
+
+for i in s:
+    if i.isalpha():
+        d[i] = d.get(i, 0) + 1
+
+
+for i in sorted(d):
+    print(i, d[i])
